adab47116c4b1d9562fdf83c0131050abaf78741
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-18 15:34:21 -0300

    req1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,7 @@
-test('Hi', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../pages/About';
+
+test('If the app has all the nav links', () => {
+  render(<About />);
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('If the app has all the nav links', () => {
+  renderWithRouter(<App />);
+
+  const homeLink = screen.getByText(/home/i);
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = screen.getByText(/about/i);
+  expect(aboutLink).toBeInTheDocument();
+
+  const favLink = screen.getByText(/favorite pokémons/i);
+  expect(favLink).toBeInTheDocument();
+});
+
+test('The Home Link', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const homeLink = screen.getByText(/home/i);
+  userEvent.click(homeLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('The About Link', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const aboutLink = screen.getByText(/about/i);
+  userEvent.click(aboutLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('The Fav Link', () => {
+  const { history } = renderWithRouter(<App />);
+
+  const favLink = screen.getByText(/favorite pokémons/i);
+  userEvent.click(favLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
