b4c138c9f73b5b2947431851e4230975461cee5b
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-18 16:54:41 -0300

    Req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+test('The Not Found page', () => {
+  render(<NotFound />);
+
+  const title = screen.getByRole('heading', { level: 2 });
+  expect(title).toHaveTextContent('Page requested not found');
+
+  const altTxt = 'Pikachu crying because the page requested was not found';
+  const img = screen.getByAltText(altTxt);
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
