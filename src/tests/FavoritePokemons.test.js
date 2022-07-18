a1f9bdd4e2b8011cac929701fd97b6d06268dc97
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-18 16:46:52 -0300

    Req3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import data from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+test('The favorite Pokemon page with no favorites', () => {
+  render(<FavoritePokemons />);
+
+  const noFaves = screen.getByText(/No favorite pokemon found/i);
+  expect(noFaves).toBeInTheDocument();
+});
+
+test('The favorite Pokemon page with favorites', () => {
+  renderWithRouter(<FavoritePokemons pokemons={ [data[0], data[1]] } />);
+
+  const faves = screen.getAllByTestId('pokemon-name');
+  expect(faves).toHaveLength(2);
+});
