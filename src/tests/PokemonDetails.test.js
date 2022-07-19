ab0fe124323ceb3b960a3ec0e1f3e015100e53cd
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-19 16:22:27 -0300

    Req 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('the Detais page', () => {
+  it('has the correct info', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(link);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+
+    const title = screen.getByRole('heading', { name: 'Pikachu Details' });
+    expect(title).toBeDefined();
+
+    const summary = screen.getByRole('heading', { name: 'Summary' });
+    const summTxt = screen.getByText(/This intelligent Pokémon/i);
+    expect(summTxt).toBeDefined();
+    expect(summary).toBeDefined();
+
+    const mapTitle = screen.getByRole('heading', { name: 'Game Locations of Pikachu' });
+    expect(mapTitle).toBeDefined();
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toBeDefined();
+    const label = screen.getByText('Pokémon favoritado?');
+    expect(label).toBeDefined();
+
+    const map = screen.getAllByAltText('Pikachu location');
+    expect(map).toHaveLength(2);
+    expect(map[0].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  });
+});
